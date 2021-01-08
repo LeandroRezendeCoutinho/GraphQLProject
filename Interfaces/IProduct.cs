ce2cb627b865c117d68d503f39f429a75c5d61fa
leandrorezendecoutinho <leandro.rc.contato@gmail.com>
2021-01-08 07:54:12 -0300

    add product interface

---
+++ Interfaces/IProduct.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using GraphQLProject.Models;
+
+namespace GraphQLProject.Interfaces
+{
+    public interface IProduct
+    {
+        List<Product> GetAllProducts();
+        Product AddProduct(Product product);
+        Product UpdateProduct(int id, Product product);
+        void DeleteProduct(int id);
+        Product GetProductById(int id);
+    }
+}
